465d77533b420a71c72c39e11560359e37580d84
UBC Student <you@example.com>
2022-07-25 18:52:11 -0700

    added semicolons

--- SQL/InsertStatements.sql
+++ SQL/InsertStatements.sql
@@ -1,32 +1,32 @@
-INSERT INTO Staff_Wage VALUES(20, 'Janitor')
-INSERT INTO Staff_Wage VALUES(25, 'Concessions')
-INSERT INTO Staff_Wage VALUES(50, 'Manager')
-INSERT INTO Staff_Wage VALUES(15, 'Intern')
-INSERT INTO Staff_Wage VALUES(100, 'CEO')
+INSERT INTO Staff_Wage VALUES(20, 'Janitor');
+INSERT INTO Staff_Wage VALUES(25, 'Concessions');
+INSERT INTO Staff_Wage VALUES(50, 'Manager');
+INSERT INTO Staff_Wage VALUES(15, 'Intern');
+INSERT INTO Staff_Wage VALUES(100, 'CEO');
 
-INSERT INTO Staff_ID VALUES(00004, 'Janitor')
-INSERT INTO Staff_ID VALUES(00003, 'Concessions')
-INSERT INTO Staff_ID VALUES(00002, 'Manager')
-INSERT INTO Staff_ID VALUES(00005, 'Intern')
-INSERT INTO Staff_ID VALUES(00001, 'CEO')
+INSERT INTO Staff_ID VALUES(00004, 'Janitor'); 
+INSERT INTO Staff_ID VALUES(00003, 'Concessions'); 
+INSERT INTO Staff_ID VALUES(00002, 'Manager'); 
+INSERT INTO Staff_ID VALUES(00005, 'Intern'); 
+INSERT INTO Staff_ID VALUES(00001, 'CEO'); 
 
-INSERT INTO Hotel_Price VALUES('123 Dubai Street', 'Marriott', 2)
-INSERT INTO Hotel_Price VALUES('456 Las Vegas', 'Hilton', 2)
-INSERT INTO Hotel_Price VALUES('789 Vancouver Street', 'Hyatt', 4)
-INSERT INTO Hotel_Price VALUES('30 Berlin Road', 'Wyndham', 3)
-INSERT INTO Hotel_Price VALUES('322 Peru Avenue', 'Choice', 4)
+INSERT INTO Hotel_Price VALUES('123 Dubai Street', 'Marriott', 2); 
+INSERT INTO Hotel_Price VALUES('456 Las Vegas', 'Hilton', 2); 
+INSERT INTO Hotel_Price VALUES('789 Vancouver Street', 'Hyatt', 4); 
+INSERT INTO Hotel_Price VALUES('30 Berlin Road', 'Wyndham', 3); 
+INSERT INTO Hotel_Price VALUES('322 Peru Avenue', 'Choice', 4); 
 
-INSERT INTO Hotel_Address VALUES('123 Dubai Street', 'Marriott', 2, 1)
-INSERT INTO Hotel_Address VALUES('456 Las Vegas', 'Hilton', 2, 2)
-INSERT INTO Hotel_Address VALUES('789 Vancouver Street', 'Hyatt', 4, 3)
-INSERT INTO Hotel_Address VALUES('30 Berlin Road', 'Wyndham', 3, 4)
-INSERT INTO Hotel_Address VALUES('322 Peru Avenue', 'Choice', 4, 5)
+INSERT INTO Hotel_Address VALUES('123 Dubai Street', 'Marriott', 2, 1); 
+INSERT INTO Hotel_Address VALUES('456 Las Vegas', 'Hilton', 2, 2); 
+INSERT INTO Hotel_Address VALUES('789 Vancouver Street', 'Hyatt', 4, 3); 
+INSERT INTO Hotel_Address VALUES('30 Berlin Road', 'Wyndham', 3, 4); 
+INSERT INTO Hotel_Address VALUES('322 Peru Avenue', 'Choice', 4, 5); 
 
-INSERT INTO Guest VALUES(11111, 'Splinter', 65, 'Sewers, NYC', 'Splinter@gmail.com', 374245455400126)
-INSERT INTO Guest VALUES(22222, 'Leonardo', 17, 'Sewers, NYC', 'Leonardo@gmail.com', NULL)
-INSERT INTO Guest VALUES(33333, 'Raphael', 17, 'Sewers, NYC', 'Raphael@gmail.com', NULL)
-INSERT INTO Guest VALUES(44444, 'Donatello', 17, 'Sewers, NYC', 'Donatello@gmail.com', NULL)
-INSERT INTO Guest VALUES(55555, 'Michelangelo', 17, 'Sewers, NYC', 'Michelangelo@gmail.com', NULL)
+INSERT INTO Guest VALUES(11111, 'Splinter', 65, 'Sewers, NYC', 'Splinter@gmail.com', 374245455400126); 
+INSERT INTO Guest VALUES(22222, 'Leonardo', 17, 'Sewers, NYC', 'Leonardo@gmail.com', NULL); 
+INSERT INTO Guest VALUES(33333, 'Raphael', 17, 'Sewers, NYC', 'Raphael@gmail.com', NULL); 
+INSERT INTO Guest VALUES(44444, 'Donatello', 17, 'Sewers, NYC', 'Donatello@gmail.com', NULL); 
+INSERT INTO Guest VALUES(55555, 'Michelangelo', 17, 'Sewers, NYC', 'Michelangelo@gmail.com', NULL); 
 
 INSERT INTO Park_Hours VALUES(1, '6:00AM - 10:00PM')
 INSERT INTO Park_Hours VALUES(2, '6:30AM - 10:00PM')
