9263fb3037a7241edb852fc20b7b11d2af134aec
UBC Student <kirbylam@students.cs.ubc.ca>
2022-07-25 19:02:04 -0700

    fixed merge

--- SQL/InsertStatements.sql
+++ SQL/InsertStatements.sql
@@ -1,3 +1,4 @@
+-- #1-5
 INSERT INTO Staff_Wage VALUES(20, 'Janitor');
 INSERT INTO Staff_Wage VALUES(25, 'Concessions');
 INSERT INTO Staff_Wage VALUES(50, 'Manager');
@@ -125,34 +126,8 @@ INSERT INTO Ride_Restriction VALUES(1, 20, 'Adult supervision required');
 INSERT INTO Ride_Restriction VALUES(6, 10, 'Must be 40 inches');
 INSERT INTO Ride_Restriction VALUES(2, 15, 'Adult supervision required');
 
-<<<<<<< HEAD
-INSERT INTO Ride_Info(8, 50, "Splash Mountain", 1);
-INSERT INTO Ride_Info(8, 30, "Matterhorn Bobsleds", 2);
-INSERT INTO Ride_Info(1, 20, "Jungle Cruise", 3);
-INSERT INTO Ride_Info(6, 10, "Radiator Springs", 4);
-INSERT INTO Ride_Info(2, 15, "Peter Pan's Flight", 5);
-=======
 INSERT INTO Ride_Info(8, 50, 'Splash Mountain', 1);
 INSERT INTO Ride_Info(8, 30, 'Matterhorn Bobsleds', 2);
 INSERT INTO Ride_Info(1, 20, 'Jungle Cruise', 3);
 INSERT INTO Ride_Info(6, 10, 'Radiator Springs', 4);
 INSERT INTO Ride_Info(2, 15, 'Peter Pan`s Flight', 5);
-
-INSERT INTO Park VALUES(1, 'Las Vegas', 'USA');
-INSERT INTO Park VALUES(2, 'Dubai', 'UAE');
-INSERT INTO Park VALUES(3, 'Vancouver', 'Canada');
-INSERT INTO Park VALUES(4, 'Lima', 'Peru');
-INSERT INTO Park VALUES(5, 'Berlin', 'Germany');
-
-INSERT INTO Type_Price VALUES('Annual-Unlimited', 1399.99);
-INSERT INTO Type_Price VALUES('Annual-Limited', 949.99);
-INSERT INTO Type_Price VALUES('1-Month Pass', 249.99);
-INSERT INTO Type_Price VALUES('1-Week Pass', 179.99);
-INSERT INTO Type_Price VALUES('1-Day Pass', 119.99);
-
-INSERT INTO GuestVisit VALUES(10001, '1-Day Pass', '2022-12-31', 11111, 1);
-INSERT INTO GuestVisit VALUES(10002, 'Annual-Unlimited', '2022-07-25', 33333, 2);
-INSERT INTO GuestVisit VALUES(10003, '1-Month Pass', '2022-01-01', 44444, 2);
-INSERT INTO GuestVisit VALUES(10004, '1-Week Pass', '2022-09-30', 22222, 3);
-INSERT INTO GuestVisit VALUES(10005, 'Annual-Unlimited', '2022-05-20', 55555, 4);
->>>>>>> 3a0935457ea1875fc2a2961550b797bc27099885
