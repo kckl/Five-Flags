8c36a544a74ada9f224e7fcb1df28770caccba9b
Chris Ting <kkting@students.cs.ubc.ca>
2022-07-25 18:26:03 -0700

    initial data for reference

--- SQL/InsertStatements.sql
+++ SQL/InsertStatements.sql
@@ -3,3 +3,21 @@ INSERT INTO Staff_Wage VALUES(25, 'Janitor')
 INSERT INTO Staff_Wage VALUES(50, 'Janitor')
 INSERT INTO Staff_Wage VALUES(15, 'Janitor')
 INSERT INTO Staff_Wage VALUES(100, 'Janitor')
+
+INSERT INTO Park VALUES(1, 'Las Vegas', 'USA');
+INSERT INTO Park VALUES(2, 'Dubai', 'UAE');
+INSERT INTO Park VALUES(3, 'Vancouver', 'Canada');
+INSERT INTO Park VALUES(4, 'Lima', 'Peru');
+INSERT INTO Park VALUES(5, 'Berlin', 'Germany');
+
+INSERT INTO Type_Price VALUES('Annual-Unlimited', 1399.99);
+INSERT INTO Type_Price VALUES('Annual-Limited', 949.99);
+INSERT INTO Type_Price VALUES('1-Month Pass', 249.99);
+INSERT INTO Type_Price VALUES('1-Week Pass', 179.99);
+INSERT INTO Type_Price VALUES('1-Day Pass', 119.99);
+
+INSERT INTO Type_Ticket_Date_Park VALUES();
+INSERT INTO Type_Ticket_Date_Park VALUES();
+INSERT INTO Type_Ticket_Date_Park VALUES();
+INSERT INTO Type_Ticket_Date_Park VALUES();
+INSERT INTO Type_Ticket_Date_Park VALUES();
