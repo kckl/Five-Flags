3a0935457ea1875fc2a2961550b797bc27099885
UBC Student <you@example.com>
2022-07-25 18:55:12 -0700

    replace " with '

--- SQL/InsertStatements.sql
+++ SQL/InsertStatements.sql
@@ -41,29 +41,29 @@ INSERT INTO Park_Category_Name VALUES(4, 'Chocolates', 'Wonka Industries')
 INSERT INTO Park_Category_Name VALUES(5, 'General', 'Walmart')
 
 -- #11-17
-INSERT INTO Stay VALUES(11111, "123 Dubai Street", "1997-01-01", "1998-01-01");
-INSERT INTO Stay VALUES(22222, "456 Las Vegas Road", "2030-03-03", "2030-03-12");
-INSERT INTO Stay VALUES(33333, "789 Vancouver Street", "2010-11-11", "2010-11-12");
-INSERT INTO Stay VALUES(44444, "30 Berlin Road", "2022-02-01", "2022-02-11");
-INSERT INTO Stay VALUES(55555, "322 Peru Avenue", "2015-09-23", "2015-09-30");
+INSERT INTO Stay VALUES(11111, '123 Dubai Street', '1997-01-01', '1998-01-01');
+INSERT INTO Stay VALUES(22222, '456 Las Vegas Road', '2030-03-03', '2030-03-12');
+INSERT INTO Stay VALUES(33333, '789 Vancouver Street', '2010-11-11', '2010-11-12');
+INSERT INTO Stay VALUES(44444, '30 Berlin Road', '2022-02-01', '2022-02-11');
+INSERT INTO Stay VALUES(55555, '322 Peru Avenue', '2015-09-23', '2015-09-30');
 
-INSERT INTO Shop VALUES(11111, "Mickey Machine");
-INSERT INTO Shop VALUES(22222, "Princess Adventure");
-INSERT INTO Shop VALUES(33333, "Disney Dive");
-INSERT INTO Shop VALUES(44444, "Mickey Trips");
-INSERT INTO Shop VALUES(55555, "Neverland Store");
+INSERT INTO Shop VALUES(11111, 'Mickey Machine');
+INSERT INTO Shop VALUES(22222, 'Princess Adventure');
+INSERT INTO Shop VALUES(33333, 'Disney Dive');
+INSERT INTO Shop VALUES(44444, 'Mickey Trips');
+INSERT INTO Shop VALUES(55555, 'Neverland Store');
 
-INSERT INTO Enjoy VALUES(11111, "Splash Mountain", 1);
-INSERT INTO Enjoy VALUES(22222, "Matterhorn Bobsleds", 2);
-INSERT INTO Enjoy VALUES(33333, "Jungle Cruise", 3);
-INSERT INTO Enjoy VALUES(44444, "Radiator Springs", 4);
-INSERT INTO Enjoy VALUES(55555, "Peter Pan's Flight", 5);
+INSERT INTO Enjoy VALUES(11111, 'Splash Mountain', 1);
+INSERT INTO Enjoy VALUES(22222, 'Matterhorn Bobsleds', 2);
+INSERT INTO Enjoy VALUES(33333, 'Jungle Cruise', 3);
+INSERT INTO Enjoy VALUES(44444, 'Radiator Springs', 4);
+INSERT INTO Enjoy VALUES(55555, 'Peter Pan`s Flight', 5);
 
-INSERT INTO Dines VALUES(11111, "Corn Dog Castle");
-INSERT INTO Dines VALUES(22222, "Popcorn Kitchen");
-INSERT INTO Dines VALUES(33333, "5 Flags Express");
-INSERT INTO Dines VALUES(44444, "The Keg");
-INSERT INTO Dines VALUES(55555, "Trader Flags Lounge");
+INSERT INTO Dines VALUES(11111, 'Corn Dog Castle');
+INSERT INTO Dines VALUES(22222, 'Popcorn Kitchen');
+INSERT INTO Dines VALUES(33333, '5 Flags Express');
+INSERT INTO Dines VALUES(44444, 'The Keg');
+INSERT INTO Dines VALUES(55555, 'Trader Flags Lounge');
 
 INSERT INTO Visit VALUES(11111, 1);
 INSERT INTO Visit VALUES(22222, 2);
@@ -77,17 +77,17 @@ INSERT INTO Works_For VALUES(00003, 3);
 INSERT INTO Works_For VALUES(00004, 4);
 INSERT INTO Works_For VALUES(00005, 5);
 
-INSERT INTO Ride_Restriction VALUES(8, 50, "Must be 44 inches");
-INSERT INTO Ride_Restriction VALUES(8, 30, "Must be 44 inches");
-INSERT INTO Ride_Restriction VALUES(1, 20, "Adult supervision required");
-INSERT INTO Ride_Restriction VALUES(6, 10, "Must be 40 inches");
-INSERT INTO Ride_Restriction VALUES(2, 15, "Adult supervision required");
+INSERT INTO Ride_Restriction VALUES(8, 50, 'Must be 44 inches');
+INSERT INTO Ride_Restriction VALUES(8, 30, 'Must be 44 inches');
+INSERT INTO Ride_Restriction VALUES(1, 20, 'Adult supervision required');
+INSERT INTO Ride_Restriction VALUES(6, 10, 'Must be 40 inches');
+INSERT INTO Ride_Restriction VALUES(2, 15, 'Adult supervision required');
 
-INSERT INTO Ride_Info(8, 50, "Splash Mountain", 1);
-INSERT INTO Ride_Info(8, 30, "Matterhorn Bobsleds", 2);
-INSERT INTO Ride_Info(1, 20, "Jungle Cruise", 3);
-INSERT INTO Ride_Info(6, 10, "Radiator Springs", 4);
-INSERT INTO Ride_Info(2, 15, "Peter Pan's Flight", 5);
+INSERT INTO Ride_Info(8, 50, 'Splash Mountain', 1);
+INSERT INTO Ride_Info(8, 30, 'Matterhorn Bobsleds', 2);
+INSERT INTO Ride_Info(1, 20, 'Jungle Cruise', 3);
+INSERT INTO Ride_Info(6, 10, 'Radiator Springs', 4);
+INSERT INTO Ride_Info(2, 15, 'Peter Pan`s Flight', 5);
 
 INSERT INTO Park VALUES(1, 'Las Vegas', 'USA');
 INSERT INTO Park VALUES(2, 'Dubai', 'UAE');
