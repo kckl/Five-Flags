244d1d7641e6bdfdb7804d700d4e4caa8f7dacd2
Chris Ting <kkting@students.cs.ubc.ca>
2022-08-02 21:35:28 -0700

    application setup

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(8000, () => {
+    console.log("Server has started on port 8000");
+});
