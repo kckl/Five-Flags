ac7832e8882cf6036a93cd8c3ca9e5f42cc8db07
Chris Ting <kkting@students.cs.ubc.ca>
2022-08-03 22:04:33 -0700

    beginning front end work

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import './Home.css';
+import img from "../images/FiveFlags.png";
 
 const Home = () => {    
     return (
         <>
-            <div className="options">
-                <h1>Welcome to Five Flags!</h1>
-                <div className="buttons">
-                    <span>Are you a </span>
-                    <Link to="/GuestView">
-                        <button type="button" onClick>Guest</button>
-                    </Link>
-                    <span> or a </span>
-                    <Link to="/StaffLogin">
-                        <button type="button" onClick>Staff</button>
-                    </Link>
-                    <span> ?</span>
+            <body>
+                <div className="container">
+                    <img src={img} alt="company logo"></img>
+                    <h1>Welcome to Five Flags!</h1>
+                    <div className="buttons">
+                        <span>Are you a </span>
+                        <Link to="/GuestView">
+                            <button type="button" onClick>Guest</button>
+                        </Link>
+                        <span> or a </span>
+                        <Link to="/StaffLogin">
+                            <button type="button" onClick>Staff</button>
+                        </Link>
+                        <span> ?</span>
+                    </div>
                 </div>
-            </div>
+            </body>
         </>
     );
 };
