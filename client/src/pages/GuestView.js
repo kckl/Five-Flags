ac4fdd52349b87a39dda435f3c5e13c492afbfea
Chris Ting <kkting@students.cs.ubc.ca>
2022-08-03 23:54:12 -0700

    planning for sql queries

--- client/src/pages/GuestView.js
+++ client/src/pages/GuestView.js
@@ -1,8 +1,15 @@
 import React from "react";
+import './GuestView.css';
 
 const GuestView = () => {
     return (
-        <h2>Hello this is the guest view</h2>
+        <>
+            <h2>Hello this is the guest view</h2>
+            <ul>
+                <li>selection operation - information dashboard: rides, shops, dining options</li>
+                <li>join operation - find names of rides with x restrictions?</li>
+            </ul>
+        </>
     );
 };
 
--- client/src/pages/StaffLogin.js
+++ client/src/pages/StaffLogin.js
@@ -2,7 +2,18 @@ import React from "react";
 
 const StaffLogin = () => {
     return (
-        <h2>Hello this is the staff view</h2>
+        <>
+            <h2>Hello this is the staff view</h2>
+            <ul>
+                <li>insert operation - add new staff</li>
+                <li>delete operation - delete rides / food establishments / shops</li>
+                <li>update operation - food establishments</li>
+                <li>aggregation with group by - total ticket sales per day / month / etc</li>
+                <li>aggregation with having - most loyal customers, i.e. having x amount of visits a year</li>
+                <li>nested aggregation with group by - something to do with ticket types</li>
+                <li>division operation - customers who have been to all our parks around the world</li>
+            </ul>
+        </>
     );
 };
 
